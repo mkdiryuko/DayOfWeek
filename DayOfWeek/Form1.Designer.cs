ab99f7bcdd9ebd99e1cd8ec50a00d97cb3fd8d71
mkdiryuko <kobayashi.yuto164@mail.kyutech.jp>
2023-11-22 21:46:17 +0900

    プロジェクト ファイルを追加します。

---
+++ DayOfWeek/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace DayOfWeek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownMonth = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownDay = new System.Windows.Forms.NumericUpDown();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.labelDayOfWeek = new System.Windows.Forms.Label();
+            this.buttonDayOfWeekCul = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦年";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "月";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "日";
+            // 
+            // numericUpDownMonth
+            // 
+            this.numericUpDownMonth.Location = new System.Drawing.Point(134, 27);
+            this.numericUpDownMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDownMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownMonth.Name = "numericUpDownMonth";
+            this.numericUpDownMonth.Size = new System.Drawing.Size(60, 22);
+            this.numericUpDownMonth.TabIndex = 3;
+            this.numericUpDownMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownDay
+            // 
+            this.numericUpDownDay.Location = new System.Drawing.Point(246, 27);
+            this.numericUpDownDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.numericUpDownDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownDay.Name = "numericUpDownDay";
+            this.numericUpDownDay.Size = new System.Drawing.Size(60, 22);
+            this.numericUpDownDay.TabIndex = 4;
+            this.numericUpDownDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.Location = new System.Drawing.Point(12, 27);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(71, 22);
+            this.textBoxYear.TabIndex = 5;
+            // 
+            // labelDayOfWeek
+            // 
+            this.labelDayOfWeek.AutoSize = true;
+            this.labelDayOfWeek.Location = new System.Drawing.Point(343, 34);
+            this.labelDayOfWeek.Name = "labelDayOfWeek";
+            this.labelDayOfWeek.Size = new System.Drawing.Size(43, 15);
+            this.labelDayOfWeek.TabIndex = 6;
+            this.labelDayOfWeek.Text = "label4";
+            // 
+            // buttonDayOfWeekCul
+            // 
+            this.buttonDayOfWeekCul.Location = new System.Drawing.Point(12, 64);
+            this.buttonDayOfWeekCul.Name = "buttonDayOfWeekCul";
+            this.buttonDayOfWeekCul.Size = new System.Drawing.Size(450, 35);
+            this.buttonDayOfWeekCul.TabIndex = 7;
+            this.buttonDayOfWeekCul.Text = "曜日算出";
+            this.buttonDayOfWeekCul.UseVisualStyleBackColor = true;
+            this.buttonDayOfWeekCul.Click += new System.EventHandler(this.buttonDayOfWeekCul_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDayOfWeekCul);
+            this.Controls.Add(this.labelDayOfWeek);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.numericUpDownDay);
+            this.Controls.Add(this.numericUpDownMonth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "曜日算出";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDay)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownMonth;
+        private System.Windows.Forms.NumericUpDown numericUpDownDay;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.Label labelDayOfWeek;
+        private System.Windows.Forms.Button buttonDayOfWeekCul;
+    }
+}
+
---
+++ DayOfWeek/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DayOfWeek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            labelDayOfWeek.Text = "";
+        }
+
+        // テキストを整数値に変換するメソッド
+        // (仮引数) text : 変換する文字列、val : 変換した整数値
+        // (返却値) (int) val
+        
+        private void textToValue(string text, out int val)
+        {
+            if (int.TryParse(text, out val) == false)
+                val = -1; // ここで-1にすることでZeller()の中で"西暦年エラー"をだしている。
+        }
+        
+
+        // 閏年かどうか判定するメソッド
+        // (仮引数) 西暦年
+        // (戻り値) 閏年なら true, そうでなければ false
+        // 「4で割り切れ、かつ100で割り切れないか、または、400で割り切れる年」は閏年
+        
+        private bool leapyearJudge(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            else if (year % 4 == 0 && year % 100 != 0)
+                return true;
+
+            return false;
+        }
+
+        // 年月日の妥当性チェックを行うメソッド
+        // (仮引数) year：年、month : 月、day：日
+        // (戻り値) 妥当ならば 空文字列, そうでなければ あり得ない日付
+        
+        private bool MonthandDayJudge(int year, int month, int day)
+        {
+            bool result = true; // 判定結果(妥当ならture, そうでなければfalse）
+
+            switch (month)
+            {
+                // 1月、3月、5月、7月、8月、10月、12月は31日まで
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    if (day > 31)
+                        result = true;
+                    break;
+                // 4月、6月、9月、11月は31日まで
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    if (day > 30)
+                        result = false;
+                    break;
+                // 2月は28日まで、閏年ならば29日まで
+                case 2:      
+                    if (day == 29 && leapyearJudge(year))  // もし閏年ならば
+                        result = true;
+                    else if (day > 28)
+                        result = false;
+                    break;
+                default:
+                    result = true;
+                    break;
+            }
+            return result;
+        }
+
+        // 西暦年、月、日から曜日を求めるメソッド(Zellerの公式)
+        // (仮引数) year : 西暦年、month : 月、day : 日
+        // (戻り値) dayofweek : 曜日（日曜=0, 月曜=1, 火曜=2,・・・）
+        
+        private string Zeller(int year, int month, int day)
+        {
+            int w;
+            string dayofweek;
+
+            if (year < 0)
+                return "西暦年エラー";
+
+            if (month == 1 || month == 2) // 1月と2月は前年の13月と14月として計算する
+            {
+                year -= 1;
+                month += 12;
+            }
+
+            w = (5 * year / 4 - year / 100 + year / 400 + (26 * month + 16) / 10 + day) % 7; // Zellerの公式
+
+            switch (w)
+            {
+                case 0:
+                    dayofweek = "日曜日です";
+                    break;
+                case 1:
+                    dayofweek = "月曜日です";
+                    break;
+                case 2:
+                    dayofweek = "火曜日です";
+                    break;
+                case 3:
+                    dayofweek = "水曜日です";
+                    break;
+                case 4:
+                    dayofweek = "木曜日です";
+                    break;
+                case 5:
+                    dayofweek = "金曜日です";
+                    break;
+                case 6:
+                    dayofweek = "土曜日です";
+                    break;
+                default: // これがないと、外部でdayofweekに何も代入していない場合、
+                         // 関数として空の変数を返す可能性があるから"未割り当てのローカル変数が使用される"というエラー発生
+                    dayofweek = "";
+                    break;
+            }
+            return dayofweek;
+        }
+
+        private void buttonDayOfWeekCul_Click_1(object sender, EventArgs e)
+        {
+            // 西暦年を整数値に変換
+            int year;
+            int month = (int)numericUpDownMonth.Value;
+            int day = (int)numericUpDownDay.Value;
+
+            textToValue(textBoxYear.Text, out year); // textを整数値に変換
+            if (MonthandDayJudge(year, month, day))
+                labelDayOfWeek.Text = Zeller(year, month, day);
+            else
+                labelDayOfWeek.Text = "あり得ない日付";
+        }
+    }
+}
